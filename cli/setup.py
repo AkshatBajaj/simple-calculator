99169d6af2730850f91f4b202ed1406b44213e79
AkshatBajaj <akshat.bajaj@outlook.com>
2020-11-04 14:26:27 +1100

    NEW: Create two separate package with their own version

---
+++ cli/setup.py
@@ -0,0 +1,18 @@
+import os
+from setuptools import setup
+
+with open(os.path.join('VERSION')) as version_file:
+    VERSION = version_file.read().strip()
+
+setup(name='cli',
+      version=VERSION,
+      description='Simple Calculator',
+      url='https://github.com/AkshatBajaj/simple-calculator',
+      author='Akshat Bajaj',
+      author_email='akshat.bajaj@outlook.com',
+      include_package_data=True,
+      packages=[
+        'cli'
+      ],
+      license="BSD-3-Clause License",
+      zip_safe=False)
